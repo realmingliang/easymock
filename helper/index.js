3f3536448e3b718a0cf73c02ad8c100ebf52ae98
brighterman <171372668@qq.com>
2021-05-10 16:13:44 +0800

    update

--- helper/index.js
+++ helper/index.js
@@ -2,6 +2,7 @@ var convertUrl = exports.convertUrl = function (url) {
   // /restful/:id/:list/{id} -> restful_id_list_id
   // /restful/:id/:list/{id}.json -> restful_id_list_id
   var _url = url
+    .replace("api",'')
     .replace(/:|{|}/g, '')
     .replace(/-/g, '_')
     .split('/')
