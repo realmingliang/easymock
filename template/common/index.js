dcedb6cb7e54798c6404856a1c7cc951708fae86
brighterman <171372668@qq.com>
2021-05-10 15:38:30 +0800

    first commit

---
+++ template/common/index.js
@@ -0,0 +1,21 @@
+< % _.forEach(config.projects, function (project) {
+    % >
+    import * as {
+      {
+        $$.convertUrl(project.name)
+      }
+    } from './{{project.name}}'; < %
+  }) % >
+
+  export {
+    < % _.forEach(config.projects, function (project, i) {
+      % > {
+        {
+          $$.convertUrl(project.name)
+        }
+      } < %
+      if (config.projects.length - 1 !== i) {
+        % > , < %
+      } % > < %
+    }) % >
+  };
---
+++ template/cover/index.js
@@ -0,0 +1,14 @@
+import { axios,convertRESTAPI } from '{{$$.relative("util")}}';
+
+<% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
+function {{$$.convertMethod(mock)}}(opts) {
+  return axios({
+    method: '{{mock.method}}',
+    url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{mock.url}}', opts)<%} else {%> '{{mock.url}}'<% } %>,
+    opts: opts
+  });
+}
+
+<% }) %>export {<% _.forEach(data.mocks, function(mock, i){ %>
+  {{$$.convertMethod(mock)}}<% if(data.mocks.length - 1 !== i) { %>,<% } %><% }) %>
+};
